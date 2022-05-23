41befda9b560ddd8bce8aac57b5c8f2763a58c65
Adebowale Otulana <otulana.adebowale@gmail.com>
2022-05-23 14:36:12 +0200

    publish test result in CI pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,10 @@ const config: Config.InitialOptions = {
   verbose: true,
   preset: 'ts-jest',
   collectCoverage: true,
+  reporters: [
+    'default',
+    ['jest-junit', { suiteName: 'Unit Tests', outputName: 'test-report.xml' }],
+  ],
 };
 
 export default config;
